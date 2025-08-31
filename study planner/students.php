e43ac9cc11b6edb866b7354ac2c7fb4b24ccafee
Amponsahmishack <130582767+Amponsahmishack@users.noreply.github.com>
2025-08-31 08:12:35 +0000

    Add files via upload

---
+++ study planner/students.php
@@ -0,0 +1,11 @@
+php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Study Planner</title>
+</head>
+<body style="text-align: center; background-color: #111; color: white;">
+    <h1>Student Study Planner</h1>
+    <img src="planner.png" alt="Student Study Planner UI" style="width:60%; border: 2px solid #fff; border-radius: 10px;">
+</body>
+</html>
